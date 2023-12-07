a2cbbab407d1990017e69af6d73759d4f422a6e9
Arianne (gh0stfvce) <114576266+gh0stfvvce@users.noreply.github.com>
2023-12-07 14:32:08 +0100

    Update webTester.py
    Añado menu interactivo

--- webTester.py
+++ webTester.py
@@ -22,24 +22,47 @@ def verificar_xss(url):
     if scripts:
         print("Posible vulnerabilidad de Cross-Site Scripting (XSS) detectada.")
 
-def escanear_vulnerabilidades(url):
+def escanear_vulnerabilidades(url, opciones):
     try:
         # Obtener el contenido HTML de la página
         response = requests.get(url)
         root = html.fromstring(response.content)
 
-        # Verificar vulnerabilidades comunes
+        # Verificar vulnerabilidades según las opciones seleccionadas
         formularios = root.xpath('//form')
         for formulario in formularios:
-            verificar_csrf(formulario)
-
-        # Agregar más verificaciones según sea necesario
-        verificar_inyeccion_sql(url)
-        verificar_xss(url)
+            if 'csrf' in opciones:
+                verificar_csrf(formulario)
+        if 'sql' in opciones:
+            verificar_inyeccion_sql(url)
+        if 'xss' in opciones:
+            verificar_xss(url)
 
     except requests.RequestException as e:
         print(f"Error al realizar la solicitud: {e}")
 
 if __name__ == "__main__":
-    url_a_escanear = "https://www.ejemplo.com"
-    escanear_vulnerabilidades(url_a_escanear)
+    url_a_escanear = input("Ingrese la URL a escanear: ")
+
+    print("Seleccione las vulnerabilidades a comprobar:")
+    print("1. CSRF")
+    print("2. Inyección SQL")
+    print("3. XSS")
+    print("4. Todas")
+
+    seleccion = input("Ingrese el número de la opción: ")
+
+    opciones_seleccionadas = []
+    if seleccion == '1':
+        opciones_seleccionadas.append('csrf')
+    elif seleccion == '2':
+        opciones_seleccionadas.append('sql')
+    elif seleccion == '3':
+        opciones_seleccionadas.append('xss')
+    elif seleccion == '4':
+        opciones_seleccionadas = ['csrf', 'sql', 'xss']
+    else:
+        print("Opción no válida. Saliendo.")
+        exit()
+
+    escanear_vulnerabilidades(url_a_escanear, opciones_seleccionadas)
